25d30e9ff4597755ccda7b8366823b05700665e6
mysteryreloaded <ntrifunovicsrb@gmail.com>
2019-11-08 20:12:22 +0100

    Add index/taxonomy page

--- theme/views/content/template/default/index.blade.php
+++ theme/views/content/template/default/index.blade.php
@@ -1,1 +1,16 @@
-{{-- Work in progress --}}
+<div class="posts">
+    @foreach ( $posts as $post )
+        <div class="post">
+            @if($post->featuredimage && !empty(get_theme_setting('content.general.featuredImage.indexPageHeight')))
+                <a class="index-image-link" href="/{{ $post->type->slug }}/{{ $post->slug }}">
+                    <div class="index-featured-image" style='background-image: url({{ $post->featuredimage->original }});'>
+                        <{{ get_theme_setting('content.general.postTitle.size') }} class="index-post-title">{{ $post->title }}</{{ get_theme_setting('content.general.postTitle.size') }}>
+                    </div>
+                </a>
+            @elseif($post->featuredimage && empty(get_theme_setting('content.general.featuredImage.indexPageHeight')))
+                <img src="{{ $post->featuredimage->original }}" class="index-featured-image img-responsive" alt="">
+            @endif
+        </div>
+    @endforeach
+</div>
+{{ $posts->links() }}
