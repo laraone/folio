616d69365fefc8498c2f7bf9064e1ac756dfc46c
mysteryreloaded <ntrifunovicsrb@gmail.com>
2019-11-11 21:51:39 +0100

    Index page refactor

--- theme/views/content/template/default/index.blade.php
+++ theme/views/content/template/default/index.blade.php
@@ -1,15 +1,17 @@
-<div class="posts">
+<div id="masonry-grid" class="posts masonry">
     @foreach ( $posts as $post )
-        <div class="post">
+        <div class="item post masonry-brick">
             @if($post->featuredimage && !empty(get_theme_setting('content.general.featuredImage.indexPageHeight')))
                 <a class="index-image-link" href="/{{ $post->type->slug }}/{{ $post->slug }}">
-                    <div class="index-featured-image" style='background-image: url({{ $post->featuredimage->original }});'>
-                        <{{ get_theme_setting('content.general.postTitle.size') }} class="index-post-title">{{ $post->title }}</{{ get_theme_setting('content.general.postTitle.size') }}>
+                    <div class="post-img" style='background-image: url({{ $post->featuredimage->original }});'>
+                        {{-- <{{ get_theme_setting('content.general.postTitle.size') }} class="index-post-title">{{ $post->title }}</{{ get_theme_setting('content.general.postTitle.size') }}> --}}
                     </div>
+                    {{-- <img src="{{ $post->featuredimage->original }}" class="post-img" alt=""> --}}
                 </a>
             @elseif($post->featuredimage && empty(get_theme_setting('content.general.featuredImage.indexPageHeight')))
-                <img src="{{ $post->featuredimage->original }}" class="index-featured-image img-responsive" alt="">
+                <img src="{{ $post->featuredimage->original }}" class="post-img" alt="">
             @endif
+            {{-- <img src="https://s3.amazonaws.com/clarifai-img/demo/889/88940833.jpg"> --}}
         </div>
     @endforeach
 </div>
